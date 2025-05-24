b4164b1c73ac6e4cb683713b1ebda60625b7034d
Rodolfo Govedarski-Roldão <r_roldao@hotmail.com>
2025-05-24 23:06:43 +0300

    End the project

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from flask import Flask
 from flask import render_template, request
 from flatmates_bill import flat
 
-
 app = Flask(__name__)
 
 
@@ -18,40 +17,70 @@ class BillFormPage(MethodView):
         bill_form = BillForm()
         return render_template("bill_form_page.html", billform=bill_form)
 
-
-class ResultsPage(MethodView):
     def post(self):
-        #Form and amounts
         billform = BillForm(request.form)
         amount = float(billform.amount.data)
         period = billform.period.data
         the_bill = flat.Bill(amount, period)
-        #First Person
+
+        # First Person
         name1 = billform.name1.data
         days_in_house1 = float(billform.days_in_house1.data)
         flatmate1 = flat.Flatmate(name1, days_in_house1)
-        #Second Person
+
+        # Second Person
         name2 = billform.name2.data
         days_in_house2 = float(billform.days_in_house2.data)
         flatmate2 = flat.Flatmate(name2, days_in_house2)
-        to_pay = flat.Flatmate.pays(flatmate1, the_bill, flatmate2)
-        amount1 = flatmate1.pays(the_bill,flatmate2)
+
+        # Finding totals
+        amount1 = flatmate1.pays(the_bill, flatmate2)
         amount2 = flatmate2.pays(the_bill, flatmate1)
-        return render_template("results.html", name1=name1,amount1=amount1,name2=name2,amount2=amount2)
+        return render_template("bill_form_page.html",
+                               billform=billform,
+                               name1=name1,
+                               amount1=round(amount1, 2),
+                               name2=name2,
+                               amount2=round(amount2, 2))
+
+
+class ResultsPage(MethodView):
+    def post(self):
+        # Form and amounts
+        billform = BillForm(request.form)
+        amount = float(billform.amount.data)
+        period = billform.period.data
+        the_bill = flat.Bill(amount, period)
+        # First Person
+        name1 = billform.name1.data
+        days_in_house1 = float(billform.days_in_house1.data)
+        flatmate1 = flat.Flatmate(name1, days_in_house1)
+        # Second Person
+        name2 = billform.name2.data
+        days_in_house2 = float(billform.days_in_house2.data)
+        flatmate2 = flat.Flatmate(name2, days_in_house2)
+        # Finding totals
+        amount1 = flatmate1.pays(the_bill, flatmate2)
+        amount2 = flatmate2.pays(the_bill, flatmate1)
+        return render_template("results.html",
+                               name1=name1,
+                               amount1=round(amount1, 2),
+                               name2=name2,
+                               amount2=round(amount2, 2))
 
 
 class BillForm(Form):
-    amount = StringField("Bill Amount: ")
-    period = StringField("Bill Period: ")
-    name1 = StringField("Name: ")
-    days_in_house1 = StringField("Days in the house: ")
-    name2 = StringField("Name: ")
-    days_in_house2 = StringField("Days in the house: ")
+    amount = StringField("Bill Amount: ", default="2000")
+    period = StringField("Bill Period: ", default="December, 2020")
+    name1 = StringField("Name: ", default="Rodolfo")
+    days_in_house1 = StringField("Days in the house: ", default="17")
+    name2 = StringField("Name: ", default="Ivana")
+    days_in_house2 = StringField("Days in the house: ", default="27")
     button = SubmitField("Calculate")
 
 
 app.add_url_rule("/", view_func=HomePage.as_view("home_page"))
-app.add_url_rule("/bill", view_func=BillFormPage.as_view("bill_form_page"))
+app.add_url_rule("/bill_form", view_func=BillFormPage.as_view("bill_form_page"))
 app.add_url_rule("/results", view_func=ResultsPage.as_view("results"))
 
 app.run(debug=True)
